9507c515116f15c06a56152a059eaf8f434ebfad
Maurice Ngicho <mrcngicho@gmail.com>
2025-04-03 01:18:57 +0300

    added enroll method

--- web_flask/routes/course.py
+++ web_flask/routes/course.py
@@ -1,7 +1,32 @@
 from flask import Blueprint, render_template
+from models import storage
+from models.course import Course
 
-course = Blueprint("course", __name__)
+course = Blueprint("course", __name__, url_prefix="/course")
 
 @course.route("/courses")
 def list_courses():
-    return render_template("courses.html")
+    courses = storage.session.query(Course).all()
+    return render_template("courses.html", courses=courses)
+
+@course.route("/home")
+def home():
+    courses = storage.session.query(Course).all()
+    return render_template("home.html", courses=courses)
+
+@course.route("/<course_id>")
+def view_course(course_id):
+    course = storage.session.get(Course, course_id)
+    if not course:
+        return "Course not found", 404
+    is_enrolled = False
+
+    return render_template("course_detail.html", course=course, is_enrolled=is_enrolled)
+
+@course.route("/<course_id>/enroll")
+def enroll(course_id):
+    """Enroll the user in the course."""
+    course = storage.session.get(Course, course_id)
+    if not course:
+        return "Course not found", 404
+    return f"You have enrolled in {course.title}!"
